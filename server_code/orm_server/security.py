11471e418dfc30fc577d4e63cfc43ea24e8431c6
Alex Sherin <al@mastercode.io>
2023-07-17 18:29:52 +0100

    vscode

---
+++ server_code/orm_server/security.py
@@ -0,0 +1,18 @@
+def has_create_permission(class_name):
+    return True
+
+
+def has_read_permission(class_name, uid):
+    return True
+
+
+def has_update_permission(class_name, uid):
+    return True
+
+
+def has_delete_permission(class_name, uid):
+    return True
+
+
+def has_search_permission(class_name):
+    return True
