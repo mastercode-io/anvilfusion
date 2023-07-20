eec7d6cad8940c7098a79dbf20774a70c6404c00
Alex Sherin <al@mastercode.io>
2023-07-20 11:49:39 +0100

    vscode

--- server_code/orm_server/persistance.py
+++ server_code/orm_server/persistance.py
@@ -6,10 +6,11 @@ from anvil.tables import app_tables
 import functools
 import re
 from copy import copy
-from importlib import import_module
+# from importlib import import_module
 from uuid import uuid4
 from datetime import datetime
 
+from ..DepManager import DepManager
 from ..orm_client.particles import ModelSearchResults
 from ..orm_client import types as orm_types
 from . import security
@@ -69,7 +70,8 @@ def get_table(class_name):
 def _get_row(class_name, module_name, uid):
     """Return the data tables row for a given object instance"""
     table = getattr(app_tables, _camel_to_snake(class_name))
-    module = import_module(module_name)
+    # module = import_module(module_name)
+    module = DepManager.get_dependency('app_model')
     cls = getattr(module, class_name)
     search_kwargs = {cls._unique_identifier: uid}
     return table.get(**search_kwargs)
@@ -127,7 +129,8 @@ def _audit_log(class_name, action, prev_row, new_row):
 def get_object(class_name, module_name, uid, max_depth=None):
     """Create a model object instance from the relevant data table row"""
     if security.has_read_permission(class_name, uid):
-        module = import_module(module_name)
+        # module = import_module(module_name)
+        module = DepManager.get_dependency('app_model')
         cls = getattr(module, class_name)
         instance = cls._from_row(
             _get_row(class_name, module_name, uid), max_depth=max_depth
@@ -142,7 +145,8 @@ def get_object(class_name, module_name, uid, max_depth=None):
 @anvil.server.callable
 def get_object_by(class_name, module_name, prop, value, max_depth=None):
     """Create a model object instance from the relevant data table row"""
-    module = import_module(module_name)
+    # module = import_module(module_name)
+    module = DepManager.get_dependency('app_model')
     cls = getattr(module, class_name)
     instance = cls._from_row(
         _get_row_by(class_name, module_name, prop, value), max_depth=max_depth
@@ -175,7 +179,8 @@ def fetch_objects(class_name, module_name, rows_id, page, page_length, max_depth
     if is_last_page:
         del anvil.server.session[rows_id]
 
-    module = import_module(module_name)
+    # module = import_module(module_name)
+    module = DepManager.get_dependency('app_model')
     cls = getattr(module, class_name)
     results = (
         [
@@ -229,7 +234,8 @@ def fetch_view(class_name, module_name, columns, search_queries, filters):
             fetch_dict[key] = build_fetch_list(key_list, key_dict)
         return q.fetch_only(*fetch_list, **fetch_dict)
 
-    mod = import_module(module_name)
+    # mod = import_module(module_name)
+    mod = DepManager.get_dependency('app_model')
     cols, links = parse_col_names(class_name, mod, columns)
 
     fetch_query = build_fetch_list(cols, links)
