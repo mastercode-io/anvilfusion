b8edaf13ba021cf913665c295a9e367a5c0d6273
Alex Sherin <al@mastercode.io>
2023-07-26 16:13:08 +0100

    vscode

--- server_code/server/utils.py
+++ server_code/server/utils.py
@@ -59,4 +59,5 @@ def init_model_enumerations(model_list):
                 for option in model_list[model]['options']:
                     option['name'] = option[name_field]
 
+    print('model_list', model_list)
     return DotDict(model_list)
