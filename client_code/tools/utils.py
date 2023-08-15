a6d6c882c41efb9e9d98d6ce48f83222bda6b03e
Alex Sherin <al@mastercode.io>
2023-08-15 13:20:13 +0100

    Alex remote commit

--- client_code/tools/utils.py
+++ client_code/tools/utils.py
@@ -60,7 +60,7 @@ def time_js_to_py(time):
 # Application environment cache
 class AppEnv:
     APP_ID = None
-    logged_user = None
+    logged_user = {}
     data_models = None
     forms = None
     views = None
