895d4c6f9f74b7b1e6ae5c88b73370a04207a7be
Alex Sherin <al@mastercode.io>
2023-08-25 16:18:00 +0100

    Alex remote commit

--- server_code/server/persistance.py
+++ server_code/server/persistance.py
@@ -237,11 +237,11 @@ def fetch_view(class_name, module_name, columns, search_queries, filters):
     cls = getattr(class_module, class_name)
     for key in filters:
         if key in cls._relationships:
-            print('Filter', key, cls._relationships.class_name)
+            print('Filter', key, cls._relationships[key].class_name)
             if isinstance(filters[key], str):
                 filters[key] = [filters[key]]
             rel_uids = [row['uid'] for row in filters[key]]
-            rel_rows = [row for row in get_table(cls._relationships.class_name).search(uid=q.any_of(*rel_uids))]
+            rel_rows = [row for row in get_table(cls._relationships[key].class_name).search(uid=q.any_of(*rel_uids))]
             filters[key] = q.any_of(*rel_rows)
     filters['tenant_uid'] = anvil.server.session.get('tenant_uid', None)
 
