50e38853a9b7f42a7dcf06c4b5ceb9ec1da51bb3
Alex Sherin <al@mastercode.io>
2023-08-28 11:36:33 +0100

    Alex remote commit

--- client_code/components/FormBase.py
+++ client_code/components/FormBase.py
@@ -243,13 +243,8 @@ class FormBase:
             print('default data')
             # instance_data = {x: self.default_data[x] for x in self.default_data 
             #                     if x in self.class_name._attributes or x in self.class_name._relationships} 
-            # print('instance data', instance_data)
             # self.data = self.class_name(**instance_data)
-            # self.data = self.class_name()
-            # for attr in instance_data:
-            #     self.data[attr] = instance_data[attr]
             self.data = self.default_data
-            print('data', self.data)
         print(self.data)
         for field in [x for x in self.form_fields if not x.is_dependent and x not in self.subforms]:
             # print(field.name, field.value)
