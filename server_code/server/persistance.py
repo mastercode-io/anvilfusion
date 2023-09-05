77a564cd0670174769f748b45b72f922f44f4c1e
Alex Sherin <al@mastercode.io>
2023-09-05 15:20:41 +0100

    Alex remote commit

--- server_code/server/persistance.py
+++ server_code/server/persistance.py
@@ -259,7 +259,6 @@ def fetch_view(class_name, module_name, columns, search_queries, filters):
     filters['tenant_uid'] = anvil.server.session.get('tenant_uid', None)
 
     rows = get_table(module_name, class_name).search(fetch_query, *search_queries, **filters)
-    # rows = get_table(module_name, class_name).search(*search_queries, **filters)
 
     return rows
 
