3f524c9fb566daf4bf632d846119c41f3af85360
Alex Sherin <al@mastercode.io>
2023-09-10 11:31:37 +0100

    Alex remote

--- server_code/server/persistance.py
+++ server_code/server/persistance.py
@@ -1,4 +1,4 @@
-import anvil
+import anvil.server
 import anvil.tables.query as q
 from anvil.server import Capability
 from anvil.tables import app_tables
@@ -167,7 +167,7 @@ def get_object_by(class_name, module_name, prop, value, max_depth=None):
 @anvil.server.callable
 def fetch_objects(class_name, module_name, rows_id, page, page_length, max_depth=None):
     """Return a list of object instances from a cached data tables search"""
-    print('Fetch objects', class_name, module_name, rows_id, page, page_length, max_depth)
+    # print('Fetch objects', class_name, module_name, rows_id, page, page_length, max_depth)
     search_definition = anvil.server.session.get(rows_id, None).copy()
     if search_definition is not None:
         class_name = search_definition.pop("class_name")
