9c98fb41007ebcc601ec315e2a920f6b046665fb
Alex Sherin <al@mastercode.io>
2023-09-18 17:02:24 +0100

    Alex remote commit

--- client_code/tools/aws.py
+++ client_code/tools/aws.py
@@ -26,33 +26,61 @@ class AmazonS3:
                 'credentials': credentials,
             },
         )
-        self.response = None
         print(f"Initialized S3 Client: {self.s3_client}")
 
-    def upload_file(self, file_name, file_body, bucket=None):
+    def upload_file(self, file_key, file_body, bucket=None):
         command = AWS.S3.PutObjectCommand({
             'Bucket': bucket or self.bucket,
-            'Key': file_name,
+            'Key': file_key,
             'Body': file_body
         })
-        self.response = self.s3_client.send(command)
-        return self.response['$metadata'].httpStatusCode == 200
+        response = self.s3_client.send(command)
+        return response['$metadata'].httpStatusCode == 200
 
-    def download_file(self, file_name, bucket=None):
+    def download_file(self, file_key, bucket=None):
         command = AWS.S3.GetObjectCommand({
             'Bucket': bucket or self.bucket,
-            'Key': file_name,
+            'Key': file_key,
         })
-        self.response = self.s3_client.send(command)
-        if self.response['$metadata'].httpStatusCode == 200:
-            return self.response['Body']
+        response = self.s3_client.send(command)
+        if response['$metadata'].httpStatusCode == 200:
+            return response['Body']
         else:
             return None
 
-    def get_presigned_url(self, file_name, bucket=None, expires_in=3600):
+    def move_file(self, file_key, new_file_key, bucket=None):
+        copy_command = AWS.S3.CopyObjectCommand({
+            'Bucket': bucket or self.bucket,
+            'CopySource': f"{bucket or self.bucket}/{file_key}",
+            'Key': new_file_key,
+        })
+        response = self.s3_client.send(copy_command)
+        if response['$metadata'].httpStatusCode == 200:
+            delete_command = AWS.S3.DeleteObjectCommand({
+                'Bucket': bucket or self.bucket,
+                'Key': file_key,
+            })
+            response = self.s3_client.send(delete_command)
+            return response['$metadata'].httpStatusCode == 204
+        else:
+            return False
+
+    def delete_files(self, file_keys, bucket=None):
+        command = AWS.S3.DeleteObjectsCommand({
+            'Bucket': bucket or self.bucket,
+            'Delete': {
+                'Objects': [{'Key': file_key} for file_key in file_keys],
+                'Quiet': True,
+            },
+        })
+        response = self.s3_client.send(command)
+        if response:
+            return response['Errors']
+
+    def get_presigned_url(self, file_key, bucket=None, expires_in=3600):
         command = AWS.S3.GetObjectCommand({
             'Bucket': bucket or self.bucket,
-            'Key': file_name,
+            'Key': file_key,
         })
         url = AWS.SignedUrl.getSignedUrl(self.s3_client, command, {'expiresIn': expires_in})
         return url
