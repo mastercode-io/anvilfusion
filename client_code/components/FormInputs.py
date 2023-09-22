bed6962b864dff8297cad073bd211aade8f45e8d
Alex Sherin <al@mastercode.io>
2023-09-23 00:31:21 +0100

    Alex remote commit

--- client_code/components/FormInputs.py
+++ client_code/components/FormInputs.py
@@ -115,11 +115,12 @@ class BaseInput:
             anvil.js.window.document.getElementById(self.container_id).innerHTML = self.html + self.shadow_label
             if self._control is None:
                 self.create_control()
-            # print('show control', self._control)
+            print('show control', self._control)
             self.control.appendTo(f"#{self.el_id}")
             self.value = self._value
             self.visible = True
             self.enabled = self._enabled
+            print('show end')
 
     def hide(self):
         if self.visible:
