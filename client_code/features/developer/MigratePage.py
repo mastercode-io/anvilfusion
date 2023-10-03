d7f6abfbef606f4cbec75b062796faf7267a7747
Alex Sherin <al@mastercode.io>
2023-10-03 21:09:45 +0100

    Alex remote commit

--- client_code/features/developer/MigratePage.py
+++ client_code/features/developer/MigratePage.py
@@ -1,29 +1,25 @@
 from ...components.PageBase import PageBase
 from ...components.FormInputs import *
+from anvil.js.window import ej
+import uuid
 
 
 class MigratePage(PageBase):
     def __init__(self, **kwargs):
         print('MigratePage')
-        self.content = """
-                <div class="container">
-                    <div class="row">
-                        <div class="col-12">
-                            <h4>Migration</h4>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-12">
-                            <p>
-                                This page is used to migrate data from the legacy system to the new system.
-                            </p>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-12">
-                            <div id="migrate-form"></div>
-                        </div>
-                    </div>
-                </div>
-            """
-        super().__init__(content=self.content, **kwargs)
+        title = 'Migrate DB Schema'
+        self.migrate_button = ej.buttons.Button({'content': 'Migrate DB', 'isPrimary': True, 'size': 'large'})
+        self.migrate_button_id = f'migrate-button-{uuid.uuid4()}'
+        self.execution_log = InlineMessage(name='execution_log')
+        self.content = f'<div id="{self.migrate_button_id}"></div>' + self.execution_log.html
+
+        super().__init__(page_title=title, content=self.content, **kwargs)
+
+
+    def form_show(self, **args):
+        print('MigratePage.form_show')
+        super().form_show(**args)
+        self.migrate_button.appendTo(f'#{self.migrate_button_id}')
+        self.execution_log.show()
+        self.execution_log.message = 'This is a test message.'
+        # self.migrate_button.on_click = self.migrate_button_click
