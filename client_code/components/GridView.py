2432b8fed473e6b2cc4c80e801095d1102817580
Alex Sherin <al@mastercode.io>
2023-10-08 16:47:19 +0100

    Alex remote commit

--- client_code/components/GridView.py
+++ client_code/components/GridView.py
@@ -329,7 +329,7 @@ class GridView:
                                                            filters=self.filters,
                                                            include_rows=False)
             self.grid['dataSource'] = self.grid_data
-            # print(self.grid_data)
+            print(self.grid_data)
             # self.grid.refresh()
         print('show grid done')
         # except Exception as e:
