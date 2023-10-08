2b2db8f204956b215d016065b50f8f2b3d2a72dd
Alex Sherin <al@mastercode.io>
2023-10-08 16:25:38 +0100

    Alex remote commit

--- client_code/components/FormBase.py
+++ client_code/components/FormBase.py
@@ -262,7 +262,7 @@ class FormBase:
             field.show()
             field.value = self.data.get(field.name) if field.name else None
             print('show field', field, field.name, field.label, field.value)
-            print('data', self.data[field.name])
+            print('data', self.data[field.name], self.data.get(field.name))
         for field in [x for x in self.form_fields if x.is_dependent]:
             field.value = self.data
             field.show()
