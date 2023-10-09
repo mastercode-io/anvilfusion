0b8221ca1494e1f82a34827672804d75d62b548b
Alex Sherin <al@mastercode.io>
2023-10-09 10:44:31 +0100

    Alex remote commit

--- client_code/datamodel/particles.py
+++ client_code/datamodel/particles.py
@@ -391,7 +391,7 @@ def _get_grid_view(cls, view_config, search_queries=None, filters=None, include_
             grid_row['row'] = row
         results.append(grid_row)
         print('get_grid_row', row, grid_row)
-        print('configuration', row.configuration)
+        print('configuration', row['configuration'])
 
     return results
 
