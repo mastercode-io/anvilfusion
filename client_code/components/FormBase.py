2bead718ff035116f2b9716b9f352b5f828be17c
Alex Sherin <al@mastercode.io>
2023-10-15 18:27:22 +0100

    Alex remote commit

--- client_code/components/FormBase.py
+++ client_code/components/FormBase.py
@@ -105,7 +105,7 @@ class FormBase:
                 },
                 {
                     'buttonModel':
-                        {'isPrimary': False, 'content': button_cancel_label, 'cssClass': 'da-cancel-button'},
+                        {'isPrimary': False, 'content': button_cancel_label, },
                     'click': self.form_cancel,
                 },
             ],
