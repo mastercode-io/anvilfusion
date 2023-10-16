32f6a863e35bf77d15711685a367f6e102a690d2
Alex Sherin <al@mastercode.io>
2023-10-16 14:17:45 +0100

    Alex remote commit

--- client_code/datamodel/particles.py
+++ client_code/datamodel/particles.py
@@ -171,7 +171,7 @@ def _constructor(attributes, relationships, computes, system_attributes):
         self.uid = kwargs.pop("uid", None)
 
         # Check that we've received arguments for all required members
-        required_args = [name for name, member in members.items() if member.required]
+        required_args = [name for name, member in members.items() if member.get('required', False)]
         for name in required_args:
             if name not in kwargs:
                 raise ValueError(f"No argument provided for required {name}")
