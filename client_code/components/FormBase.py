ffe8a80d0c7367a5fee1695c715c1e397f2c6095
Alex Sherin <al@mastercode.io>
2023-10-17 15:08:29 +0100

    Alex remote commit

--- client_code/components/FormBase.py
+++ client_code/components/FormBase.py
@@ -269,7 +269,7 @@ class FormBase:
             # self.data = self.default_data
         print(self.data)
         for field in [x for x in self.form_fields if not x.is_dependent and x not in self.subforms]:
-            print(field.name, (field.name in self.data))
+            print(field.name)
             field.show()
             if field.name and getattr(self.data, field.name, None):
                 field.value = self.data[field.name]
