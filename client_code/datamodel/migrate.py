cd3c31b27915c9c0bf71c04596e8ec0119f605a3
Alex Sherin <al@mastercode.io>
2023-10-19 11:09:03 +0100

    Alex remote commit

--- client_code/datamodel/migrate.py
+++ client_code/datamodel/migrate.py
@@ -101,7 +101,7 @@ def update_model(class_name, force_update=False, self_ref=False):
             
             # add linked sample rows
             for ref_name, ref_obj in cls._relationships.items():
-                # print(ref_name, ref_obj, self_ref)
+                print(ref_name, ref_obj, self_ref)
                 
                 if self_ref is not True:
                     ref_sample, ref_refs, ref_log = update_model(ref_obj.class_name, force_update=True, self_ref=False)
