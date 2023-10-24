63f11d620905cc259868ddabbdb8109a22a8ce08
Alex Sherin <al@mastercode.io>
2023-10-24 14:40:56 +0100

    Alex remote commit

--- client_code/tools/utils.py
+++ client_code/tools/utils.py
@@ -219,15 +219,17 @@ class AppEnv:
     def set_tenant(tenant_uid=None, tenant_name=None, reload_func=None):
         print(AppEnv, AppEnv.logged_user)
         if AppEnv.logged_user.permissions.super_admin or AppEnv.logged_user.permissions.developer:
-            AppEnv.logged_user = anvil.server.call('set_tenant',
-                                                   tenant_uid=tenant_uid,
-                                                   tenant_name=tenant_name)
+            logged_user = anvil.server.call('set_tenant',
+                                            tenant_uid=tenant_uid,
+                                            tenant_name=tenant_name)
+            AppEnv.logged_user = DotDict(logged_user)
         if callable(reload_func):
             reload_func()
 
     @staticmethod
     def reset_tenant(reload_func=None):
         if AppEnv.logged_user.permissions.super_admin or AppEnv.logged_user.permissions.developer:
-            AppEnv.logged_user = anvil.server.call('set_tenant')
+            logged_user = anvil.server.call('set_tenant')
+            AppEnv.logged_user = DotDict(logged_user)
         if callable(reload_func):
             reload_func()
