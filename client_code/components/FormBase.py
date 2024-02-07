141bdc67cab287ca878bcaf43014d07562a81bac
alexsherin <alex.sherin@gmail.com>
2024-02-07 17:49:46 +0000

    Alex dev commit

--- client_code/components/FormBase.py
+++ client_code/components/FormBase.py
@@ -295,7 +295,7 @@ class FormBase:
         for field in self.form_fields:
             # print('on_change', field.name, field.value, field.on_change)
             if field.on_change is not None:
-                # print('on_change', field.name)
+                print('on_change', field.name)
                 field.on_change({'name': field.name, 'value': field.value})
         self.container_el.style.visibility = 'visible'
         self.form.cssClass = 'e-fixed py-dialog'
