67a4f2d8aa8c32dffa864d43902862395cc55531
alexsherin <alex.sherin@gmail.com>
2024-03-05 19:00:48 +0000

    Alex dev commit

--- client_code/components/GridView.py
+++ client_code/components/GridView.py
@@ -345,7 +345,7 @@ class GridView:
         if self.grid_height < 0:
             self.grid_height = None
         print('grid height', self.grid_height, self.container_el.offsetHeight, GRID_HEIGHT_OFFSET)
-        if self.grid_height > 0:
+        if self.grid_height:
             self.container_el.innerHTML = f'\
                 <div id="da-grid-container" style="height:{self.grid_height}px;">\
                     <div id="{self.grid_el_id}"></div>\
