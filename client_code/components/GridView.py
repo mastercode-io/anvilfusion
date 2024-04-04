c98a5123f1a3e892e5b4ea058e71ea8fbe12c058
alexsherin <alex.sherin@gmail.com>
2024-04-04 18:35:41 +0100

    Alex auto-save

--- client_code/components/GridView.py
+++ client_code/components/GridView.py
@@ -432,8 +432,8 @@ class GridView:
         # for k in self.grid.keys():
         #     print(k, self.grid[k])
         print(self.grid.toolbarModule)
-        for k in self.grid.toolbarModule.toolbar.properties.items[0].keys():
-            print(k, self.grid.toolbarModule.toolbar.properties.items[0][k])
+        for k in self.grid.toolbarModule.toolbar.properties.items[1].keys():
+            print(k, self.grid.toolbarModule.toolbar.properties.items[1][k])
         print('show grid done')
         # except Exception as e:
         #     print('Error in Grid form_show', e)
