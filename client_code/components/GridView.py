8226734717ed9b9bc34c9b0eadce3ffb6e65e609
alexsherin <alex.sherin@gmail.com>
2024-04-09 17:34:20 +0100

    Alex auto-save

--- client_code/components/GridView.py
+++ client_code/components/GridView.py
@@ -232,7 +232,7 @@ class GridView:
                         'width': column.get('width', None) or GRID_DEFAULT_COLUMN_WIDTH,
                     }
                 else:
-                    # print('column', column['name'])
+                    print('column', column['name'])
                     col_attr, _ = get_model_attribute(self.model, column['name'])
                     if '.' in column['name']:
                         if col_attr.field_type == dmtypes.FieldTypes.OBJECT and col_attr.schema:
