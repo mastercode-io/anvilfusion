6d36a0d6547672d517ab9ae79e5fd61c1a0bc37c
alexsherin <alex.sherin@gmail.com>
2024-04-14 22:39:32 +0100

    Alex auto-save

--- client_code/components/FormInputs.py
+++ client_code/components/FormInputs.py
@@ -982,10 +982,12 @@ class InlineMessage(BaseInput):
         if label_el_id is not None:
             self._label_style_source = label_el_id
         if self._label_style_source is not None:
+            print('setting label style', self._label_style_source)
             source_el = anvil.js.window.document.getElementById(label_el_id)
             target_el = anvil.js.window.document.getElementById(f'label_{self.el_id}')
             computed_styles = anvil.js.window.getComputedStyle(source_el)
             for style_name in computed_styles:
+                print(style_name, computed_styles[style_name])
                 try:
                     target_el.style[style_name] = computed_styles[style_name]
                 except Exception as e:
