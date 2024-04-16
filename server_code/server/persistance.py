38b7d292f0663cf74213bdc064f248f86f8a0a8c
alexsherin <alex.sherin@gmail.com>
2024-04-16 18:32:26 +0100

    Alex auto-save

--- server_code/server/persistance.py
+++ server_code/server/persistance.py
@@ -243,7 +243,8 @@ def fetch_objects(class_name, module_name, rows_id, page, page_length, max_depth
     logged_user = get_logged_user(background_task_id=background_task_id)
     user_permissions = get_user_permissions(logged_user=logged_user)
     search_definition = anvil.server.session.get(rows_id, None).copy()
-    if search_definition is not None and 'tenant_uid' not in search_definition.keys():
+    # if search_definition is not None and 'tenant_uid' not in search_definition.keys():
+    if search_definition is not None:
         if not user_permissions['super_admin']:
             search_definition['tenant_uid'] = logged_user.get('tenant_uid', None)
         else:
