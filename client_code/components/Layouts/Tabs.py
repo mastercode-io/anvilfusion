1a7641982ce9719ae65f7d65ba3ab52eb9c0039e
alexsherin <alex.sherin@gmail.com>
2024-05-20 16:54:31 +0100

    Alex auto-save

---
+++ client_code/components/Layouts/Tabs.py
@@ -0,0 +1,49 @@
+import anvil.js
+from anvil import BlobMedia
+from anvil.js.window import jQuery, ej
+# from ..tools.utils import AppEnv, DotDict, new_el_id, label_to_id
+import uuid
+
+
+class Tabs:
+    def __init__(self,
+                 container_id=None,
+                 tabs_config=None,
+                 selected_item=0,
+                 **kwargs):
+
+        self.container_id = container_id or f'tabs-container-{uuid.uuid4()}'
+        self.tabs_id = f'tabs-control-{uuid.uuid4()}'
+        self.tabs_config = tabs_config or []
+        self.selected_tab = selected_item
+        self.tabs = None
+
+        self.items = {}
+        for tab in self.tabs_config:
+            item = {
+                'id': tab.get('id') or f"tab-{tab['name']}-{uuid.uuid4()}",
+                'label': tab.get('label') or 'Tab',
+                'content_id': f"tab-{tab['name']}-content-{uuid.uuid4()}",
+                'content': tab.get('content') or '',
+                'enabled': tab.get('enabled') or True
+            }
+            self.items[item['name']] = item
+
+        self.html = self.tabs_content()
+
+
+    def tabs_content(self):
+        html = f'<div id="{self.tabs_id}">'
+        for item in self.items:
+            html += f'<div id="{item["content_id"]}"">{item["content"]}</div>'
+        html += '</div>'
+        return html
+
+
+    def form_show(self):
+        anvil.js.window.document.getElementById(self.container_id).innerHTML = self.html
+        self.tabs = ej.navigations.Tab({
+            'items': self.items,
+            'selected': self.selected_tab
+        })
+        self.tabs.appendTo(f"#{self.tabs_id}")
