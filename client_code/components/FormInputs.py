3ac2e02ab4da18c62ecdf49e4949c5f78e14df35
alexsherin <alex.sherin@gmail.com>
2024-05-27 17:15:58 +0100

    Alex auto-save

--- client_code/components/FormInputs.py
+++ client_code/components/FormInputs.py
@@ -809,10 +809,10 @@ class DropdownInput(BaseInput):
             elif self.inplace_mode is not None and self.visible:
                 value_text = next((item[self.text_field] for item in self.options
                                    if item[self.value_field] == value), '')
-                self.control.element.querySelector('.e-editable-value').style.display = 'none'
+                self.control.element.querySelector('.e-editable-value').style.opacity = 1
                 self.control.value = value
                 self.control.element.querySelector('.e-editable-value').innerText = value_text
-                self.control.element.querySelector('.e-editable-value').style.display = 'inline-block'
+                self.control.element.querySelector('.e-editable-value').style.opacity = 1
 
     @property
     def options(self):
