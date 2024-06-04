7f8d5802d85ffc9427e09ac4a483e14071815c21
alexsherin <alex.sherin@gmail.com>
2024-06-04 18:00:36 +0100

    Alex auto-save

--- client_code/components/GridView.py
+++ client_code/components/GridView.py
@@ -406,6 +406,8 @@ class GridView:
         if self.grid_height < 0:
             self.grid_height = None
         print('grid height A', self.grid_height, container_top, viewport_height)
+        print(self.container_el.offsetHeight, self.container_el.style.height)
+        print(self.container_el.getBoundingClientRect().top, self.container_el.getBoundingClientRect().bottom)
         # print(self.container_el.id, self.container_el.offsetHeight, self.container_el.style.height)
         # parent_el = self.container_el.parentNode
         # print(parent_el.id, 'o', parent_el.offsetHeight, 'h', parent_el.parentNode.style.height)
