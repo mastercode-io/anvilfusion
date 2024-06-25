2406d9ba51296b218780708e73da863e8ce08ddd
alexsherin <alex.sherin@gmail.com>
2024-06-25 14:10:24 +0100

    Alex auto-save

--- server_code/server/persistance.py
+++ server_code/server/persistance.py
@@ -27,7 +27,7 @@ def caching_query(search_function):
     def wrapper(
             class_name, module_name, page_length, page, max_depth, with_class_name, background_task_id, **search_args
     ):
-        # print('caching_query', search_args)
+        print('caching_query', search_args)
         logged_user = get_logged_user(background_task_id=background_task_id)
         user_permissions = get_user_permissions(logged_user=logged_user)
         all_tenants = False
